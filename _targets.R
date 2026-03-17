abad5f6a2a06564efe17889b20c5e1516a315049
Johan Sebastian Ohlendorff <johan.ohlendorff@sund.ku.dk>
2026-03-17 13:03:07 +0100

    Add continuous-time estimation option

--- _targets.R
+++ _targets.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: okt 23 2025 (15:22) 
 ## Version: 
-## Last-Updated: mar 16 2026 (08:06) 
-##           By: Thomas Alexander Gerds
-##     Update #: 24
+## Last-Updated: Mar 17 2026 (12:57) 
+##           By: Johan Sebastian Ohlendorff
+##     Update #: 64
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -19,11 +19,31 @@ library(targets)
 library(lava)
 library(data.table)
 tar_source("functions")
-tar_option_set(packages = c("lava","survival","data.table","prodlim","rtmle","foreach","ggplot2","plotly"))
+tar_option_set(packages = c("lava","survival","data.table","prodlim","rtmle","foreach","ggplot2","plotly", "contICEIPCW"))
+## Install:
+## devtools::install_github("jsohlendorff/contICEIPCW")
+
 list(
     tar_target(name = diabetes_polypharmacy_setting,{
         command =
             get_diabetes_polypharmacy_setting()
+    }),
+    ## Example for generating data from interventional distribution:
+    ## Treatments: GLP1, SGLT2, DPP4
+    ## All start on GLP1 at baseline.
+    ## Afterwards, the treatment regimen states that patients
+    ## can stay on GLP1, but may not take SGLT2 at any time;
+    ## Values of DPP4 are not intervened upon.
+    tar_target(name = diabetes_polypharmacy_setting_interventional_data,{
+        command = {
+            diabetes_polypharmacy_setting_intervene <- diabetes_polypharmacy_setting
+            diabetes_polypharmacy_setting_intervene$intervene <- TRUE
+            diabetes_polypharmacy_setting_intervene$intervene_variables <- c("GLP1", "SGLT2") 
+            diabetes_polypharmacy_setting_intervene$intervene_values <- c(1, 0)
+            simulate_diabetes_population(diabetes_polypharmacy_setting = diabetes_polypharmacy_setting_intervene,
+                                         initial_treatment = list(GLP1 = 2801,SGLT2 = 1577,DPP4 = 3304))
+        }
+            
     }),
     tar_target(name = diabetes_population,{ 
         command ={
@@ -34,10 +54,14 @@ list(
     tar_target(name = rtmle_diabetes_population,
                command = {
                    run_rtmle_diabetes_population(diabetes_population = diabetes_population)
+               }),
+    tar_target(name = ice_ipcw_diabetes_population,
+               command = {
+                   run_ice_ipcw(data = diabetes_population, time_horizon = 60, regimens = c("GLP1", "SGLT2", "DPP4"))
                })
 )
 
 
 
 ######################################################################
-### _targets.R ends here
+### _targets.R ends her
---
+++ functions/ice_ipcw.R
@@ -0,0 +1,81 @@
+### ice_ipcw.R --- 
+#----------------------------------------------------------------------
+## Author: Johan Sebastian Ohlendorff
+## Created: Mar 16 2026 (11:52) 
+## Version: 
+## Last-Updated: Mar 17 2026 (13:00) 
+##           By: Johan Sebastian Ohlendorff
+##     Update #: 94
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+
+run_ice_ipcw <- function(data, time_horizon, regimens = c("GLP1", "SGLT2", "DPP4")){
+    ## require(contICEIPCW) ##devtools::install_github("jsohlendorff/contICEIPCW")
+    model_hazard <- "learn_coxph"
+    setkeyv(data, c("id", "time"))
+    baseline_data <- data[time == 0, c("id", "sex", "age", "HbA1c", "U", regimens), with = FALSE]
+    setnames(baseline_data, regimens, paste0(regimens, "_0"))
+    timevar_data <- data[time > 0, c("id", "time", "event", "changeHbA1c", regimens), with = FALSE]
+    ## Change labels visit, MACE, death, dropout to A, Y, D, C
+    timevar_data[event == "visit", event := "A"]
+    timevar_data[event == "MACE", event := "Y"]
+    timevar_data[event == "death", event := "D"]
+    timevar_data[event == "dropout", event := "C"]
+    ## Remove events after event==Y;
+    ## Only first MACE event matters for the analysis.
+    ## MACE cannot occur after first event if it was not already mace?
+    terminal_time <- timevar_data[event%in% c("C", "D", "Y")][, .(terminal_time = min(time)), by = "id"]
+    timevar_data <- merge(timevar_data, terminal_time, by = "id", all.x = TRUE)
+    timevar_data <- timevar_data[time <= terminal_time]
+    timevar_data[, terminal_time:= NULL]
+    timevar_data[, event := as.factor(event)]
+
+    res <- list()
+    ## Run ICE-IPCW estimator across all regimens
+    for (regimen in regimens){
+        other_regimens <- setdiff(regimens, regimen)
+        other_regimens_baseline <- paste0(other_regimens, "_0")
+        data_regimen <- copy(timevar_data)[, !(other_regimens), with = FALSE]
+        baseline_regimen <- copy(baseline_data)[, !(other_regimens_baseline), with = FALSE]
+        setnames(data_regimen, regimen, "A")
+        setnames(baseline_regimen, paste0(regimen, "_0"), "A_0")
+        prep_data <- contICEIPCW::prepare_data(
+            data = list(baseline_data = baseline_regimen,
+                        timevarying_data = data_regimen),
+            max_time_horizon = time_horizon,
+            time_covariates = c("changeHbA1c", "A"),
+            baseline_covariates =  c("age", "A_0", "sex", "HbA1c", "U"),
+            marginal_censoring = TRUE
+        )
+        prop_scores <- contICEIPCW::propensity_scores(
+            prepared_data = prep_data,
+            model_treatment = "learn_glm_logistic",
+            penalize_treatment = TRUE,
+            model_hazard = "learn_coxph"
+        )
+        est <- contICEIPCW::debias_ice_ipcw(
+            prepared_data = prop_scores,
+            time_horizon = time_horizon,
+            model_pseudo_outcome = "lm",
+            penalize_pseudo_outcome = TRUE,
+            model_hazard = NULL,
+            penalize_hazard = FALSE,
+            conservative = TRUE,
+            static_intervention = 1,
+            semi_tmle = TRUE,
+            verbose = TRUE
+        )
+        est$intervention <- paste0("stay_on_", regimen)
+        res[[regimen]] <- est
+    }
+    res
+}
+
+######################################################################
+### ice_ipcw.R ends here
