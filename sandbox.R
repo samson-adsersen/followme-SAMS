95fa24d5fdc6df27f19a7f20822c6f1777db7333
Thomas Alexander Gerds <tag@biostat.ku.dk>
2026-03-18 10:42:39 +0100

    nothing

--- sandbox.R
+++ sandbox.R
@@ -3,7 +3,7 @@ library(plotly)
 tar_load_globals()
 tar_load_everything()
 dps <- diabetes_polypharmacy_setting
-dps$parameter_values <- modifyList(dps$parameter_values,list(effect_GLP1_MACE = -2,scale_MACE = 0.002))
+dps$parameter_values <- modifyList(dps$parameter_values,list(effect_GLP1_MACE = 0.7,scale_MACE = 0.002))
 d <- simulate_diabetes_population(diabetes_polypharmacy_setting = dps,initial_treatment = list(GLP1 = 801,SGLT2 = 1077,DPP4 = 1304))
 setkey(d,id,time,event)
 # find censored
@@ -15,6 +15,7 @@ bsl[,event := NULL]
 bsl <- event_time_data[bsl,on = "id"]
 bsl[,treatment := factor(GLP1+2*SGLT2+3*DPP4,levels = 1:3,labels = c("GLP1","SGLT2","DPP4"))]
 ggplot(bsl,aes(x = time,event = event,color = treatment))+geom_prodlim(cause = "MACE",cens.code = "censored",conf_int = 0L)+xlim(c(0,50))
+plot(prodlim(Hist(time,event,cens.code = "censored")~treatment,data = bsl),cause = "MACE",xlim = c(0,10))
 
 x <- run_rtmle_diabetes_population(diabetes_population = d,time_horizon = 1:10)
 
